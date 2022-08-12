01630c080b36d57b655ff5de7ab622cca0d65fbd
Niklas <nickbonn007@googlemail.com>
2022-08-12 16:00:21 +0200

    Anzeige des csv Inhalts Funktioniert (hoffentlich)

--- ChangeCSV.py
+++ ChangeCSV.py
@@ -1,4 +1,5 @@
-import csv;
+import csv
+import os
 
 def GetFileContent(path):
     with open(path, newline='') as csvfile:
@@ -6,6 +7,10 @@ def GetFileContent(path):
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect)
         return reader
+
+def ShowContent(csvReader):
+    for row in csvReader:
+        print(row + '\n')
 
 path = input("Pfad angeben:\n\n")
 
@@ -18,6 +23,19 @@ print('[1]Einen Wert hinzufuegen')
 print('[2]Einen Wert loeschen')
 print('[3]Ein Attribut hinzufuegen')
 choiceIndex = input()
+
+#nicht sicher ob das klappt aber eigentlich sollte es die Console Clearen
+clear = lambda: os.system('cls')
+clear()
+
+if(choiceIndex == 0):
+    ShowContent(csvReader)
+elif(choiceIndex == 1):
+    print(1)
+elif(choiceIndex == 2):
+    print(2)
+elif(choiceIndex == 3):
+    print(3)
 
 
 
