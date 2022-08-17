d705362ba58d7e9292b59c50b071500fa7c7f726
Atilgan <datilgan@goerg.local>
2022-08-17 20:08:02 +0200

    writer ergänzt in testcsv

--- csvTests.py
+++ csvTests.py
@@ -1,9 +1,10 @@
 import csv
+from lib2to3.pgen2.token import NEWLINE
 import os
 
-def csvHandler(path):
+def csvHandler(path, newzeile):
     with open(path) as csvfile:
-        csvreader = csv.reader(csvfile)
+        csvreader = csv.reader(csvfile, newline='')
         header = []
         header = next(csvreader) #returned die erste Zeile und springt zur nächsten
         datacore = []
@@ -11,8 +12,19 @@ def csvHandler(path):
             datacore.append(row)
     print("header:", header)
     print("daten:", datacore)
+    if newzeile != False or newzeile != '':
+        csvwriter = csv.writer(csvfile)
+        teile = newzeile.split(' ')
+        neueAttribute = []
+        for teil in teile:
+            neueAttribute.append(teil)
+        csvwriter.writerow(neueAttribute)
+        
     return "it worked"
 
-pfad = input("Bitte gib Dateipfad an:")      
-print(csvHandler(pfad))
+pfad = input("Bitte gib Dateipfad an:")    
+daten = input("gib neue Daten für Name, Alter und CoolJaNein an:")  
+print(csvHandler(pfad, daten))
+
+
 print("ENDE")       
