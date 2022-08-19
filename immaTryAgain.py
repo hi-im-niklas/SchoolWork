37abdd2a0f3236198b0f1471da2f3b3a9bc7b8b6
Atilgan <datilgan@goerg.local>
2022-08-19 10:00:28 +0200

    schalallalalal

--- immaTryAgain.py
+++ immaTryAgain.py
@@ -2,9 +2,32 @@ import csv
 import os
 
 def GetFileContentInformation(path):
-    with open(path, newline='') as csvfile:
+    with open(path, "r", newline='') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
-        csvreader = csv.reader(csvfile)
+        csvreader = csv.reader(csvfile, dialect=dialect)
+        datacore = []
         if csv.Sniffer().has_header(csvfile.read(1024)):
             header = []
             header = next(csvreader)
+            for row in csvreader:
+                datacore.append(row)
+            return datacore, header
+        else:
+            for row in csvreader:
+                datacore.append(row)
+            return datacore
+    
+def ManipulateFileContents(path):
+    
+    
+          
+pfad = input("Geben Sie den Dateipfad an:")            
+
+print('Was moechten sie mit der Datei machen')
+print('[0]Auslesen')
+print('[1]Einen Wert hinzufuegen')
+print('[2]Einen Wert loeschen')
+choiceIndex = input()
+
+if choiceIndex == 0:
+    GetFileContentInformation(pfad)
